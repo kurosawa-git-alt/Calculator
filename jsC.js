928ff4f405ea8b078e65d78f5e5614c2edd2a40b
EC2 Default User <ec2-user@ip-172-31-24-254.us-east-2.compute.internal>
2021-02-27 03:25:05 +0000

    redo

--- jsC.js
+++ jsC.js
@@ -1,13 +1,34 @@
 var result = document.getElementById("inputDisplay");
+totalNum = 0;
+display = "";
+tInput = "+";
+judg = 1;
 
 function num(val) {
-    result.value += val.value;
+    judg = 0;
+    display += val.value;
+    result.value = display;
 }
 
 function mark(val) {
-    result.value += val.value;
+    if(judg == 0){
+        judg = 1;
+        calc = totalNum + tInput + display;
+        totalNum = (new Function("return " + calc))();
+        display = "";
+        result.value = totalNum;
+    }
+    if(val == "=") {
+        totalNum = 0;
+        tInput = "+";
+    }else{
+        tInput =val.value;
+    }
 }
 
-function equal() {
-    result.value = new Function("return "+ result.value)();
+function power() {
+    totalNum = 0;
+    tInput = "+";
+    display = "";
+    result.value = totalNum;
 }
